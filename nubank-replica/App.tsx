4f0d964bb6ccac2de4936d1224fb9bffef371834
paulocesar-neto <paulo.valeneto@gmail.com>
2026-06-14 17:55:43 -0300

    adicionando P3 da matéria de React Native

---
+++ nubank-replica/App.tsx
@@ -0,0 +1,229 @@
+import { Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { styles } from './styles';
+import Eu from './assets/Eu.jpeg';
+import olho1 from './assets/olho1.png';
+import olho2 from './assets/olho2.png';
+import seta from './assets/seta.png';
+import Pix from './assets/Pix.png';
+import recarga from './assets/recarga.png';
+import pagar from './assets/pagar.png';
+import emprestimo from './assets/emprestimo.png';
+import caixinha from './assets/caixinha.png';
+import ajuda from './assets/ajuda.png';
+import card from './assets/card.png';
+import info from './assets/info.png';
+import options from './assets/options.png';
+import { useState } from 'react';
+
+export default function App() {
+  const [showSaldo, setShowSaldo] = useState(false);
+  
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+    <View style={styles.container}>
+      <View style={styles.navBar}>
+        <View style={styles.images}>
+          <Image style={styles.photoPerfil}
+            source={Eu} />
+          <View style={styles.navIcon}>
+            <TouchableOpacity 
+              onPress={() => setShowSaldo (!showSaldo)}>
+                <Image style={styles.eyeIcon}
+                source={showSaldo ? olho1 : olho2} />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image style={styles.eyeIcon}
+                source={info} />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image style={styles.eyeIcon}
+                source={options} />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <Text style={styles.welcomeText}>
+          Olá, Paulo
+        </Text>
+      </View>
+      <View style={styles.infoConta}>
+
+        <TouchableOpacity>
+          <View style={styles.txtIconContainer}>
+            <Text style={styles.txtAccount}>
+              Saldo em conta
+            </Text>
+            <Image style={styles.setaIcon}
+              source={seta} />
+          </View>
+          <Text style={styles.accountValue}>
+            {showSaldo ? 'R$ 300.000,00': '••••••••'}
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.buttonsDiv}>
+        <ScrollView horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.buttons}>
+
+          <TouchableOpacity style={styles.buttonsIcons}>
+            <View style={styles.buttonNav}>
+              <Image style={styles.imgIcon}
+                source={Pix} />
+            </View>
+            <Text style={styles.buttonTxt}>
+              Área Pix e Transferir
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.buttonsIcons}>
+
+            <View style={styles.pagarButton}>
+              <View style={styles.buttonNav}>
+                <Image style={styles.pagarIcon}
+                  source={pagar} />
+              </View>
+              <Text style={styles.buttonTxt}>
+                Pagar
+              </Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.buttonsIcons}>
+            <View style={styles.buttonNav}>
+              <Image style={styles.imgIcon}
+                source={emprestimo} />
+            </View>
+            <Text style={styles.buttonTxt}>
+              Pegar emprestado
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.buttonsIcons}>
+            <View style={styles.buttonNav}>
+              <Image style={styles.imgIcon}
+                source={ajuda} />
+            </View>
+            <Text style={styles.buttonTxt}>
+              Ajuda financeira
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.buttonsIcons}>
+            <View style={styles.buttonNav}>
+              <Image style={styles.imgIcon}
+                source={recarga} />
+            </View>
+            <Text style={styles.buttonTxt}>
+              Recarga de celular
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.buttonsIcons}>
+            <View style={styles.buttonNav}>
+              <Image style={styles.imgIcon}
+                source={caixinha} />
+            </View>
+            <Text style={styles.buttonTxt}>
+              Caixinhas e Investir
+            </Text>
+          </TouchableOpacity>
+        </ScrollView>
+
+        <TouchableOpacity style={styles.cardDiv}>
+          <Image style={styles.imgCard}
+            source={card} />
+          <Text style={styles.cardTxt}>
+            Meus cartões
+          </Text>
+        </TouchableOpacity>
+        <View style={styles.line}>
+        </View>
+      </View>
+
+      <View style={styles.infoCard}>
+        <TouchableOpacity>
+          <View style={styles.txtIconContainer2}>
+            <Text style={styles.txtCard}>
+              Cartão de crédito
+            </Text>
+            <Image style={styles.setaIcon}
+              source={seta} />
+          </View>
+        </TouchableOpacity>
+          <Text style={styles.cardInfo}>
+            Fatura atual
+          </Text>
+        <Text  style={styles.cardValue} >
+          {showSaldo ? 'R$ 0,00': '••••••••'}
+        </Text>
+        <Text style={styles.cardInfo}>
+            Limite disponível de {showSaldo ? 'R$ 7.000,00': '••••••••'}
+          </Text>
+          <View style={styles.line}>
+        </View>
+           <TouchableOpacity>
+          <View style={styles.txtIconContainer2}>
+            <Text style={styles.txtCard}>
+              Empréstimo
+            </Text>
+            <Image style={styles.setaIcon}
+              source={seta} />
+          </View>
+        </TouchableOpacity>
+          <Text style={styles.cardInfo}>
+            Até R$ {showSaldo ? 'R$ 10.000,00': '••••••••'} disponível para você
+          </Text>
+            <View style={styles.line}>
+        </View>
+           <TouchableOpacity>
+          <View style={styles.txtIconContainer2}>
+            <Text style={styles.txtCard}>
+              Seguro de Vida
+            </Text>
+            <Image style={styles.setaIcon}
+              source={seta} />
+          </View>
+        </TouchableOpacity>
+          <Text style={styles.cardInfo}>
+          Um seguro completo que cabe no bolso
+          </Text>
+             <View style={styles.line}>
+        </View>
+           <TouchableOpacity>
+          <View style={styles.txtIconContainer2}>
+            <Text style={styles.txtCard}>
+              Investimento
+            </Text>
+            <Image style={styles.setaIcon}
+              source={seta} />
+          </View>
+        </TouchableOpacity>
+          <Text style={styles.cardInfo}>
+          Conheça nossos fundos de investimentos
+          </Text>
+               <View style={styles.line}>
+        </View>
+           <TouchableOpacity>
+          <View style={styles.txtIconContainer2}>
+            <Text style={styles.txtCard}>
+              Shopping
+            </Text>
+            <Image style={styles.setaIcon}
+              source={seta} />
+          </View>
+        </TouchableOpacity>
+          <Text style={styles.cardInfo}>
+            Vantagens exclusivas das nossas marcas preferidas
+          </Text>
+          
+
+      </View>
+      
+    </View>
+    </ScrollView>
+  );
+}
+
+
---
+++ nubank-replica/styles.ts
@@ -0,0 +1,163 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fcfcfc',
+  },
+  navBar: {
+    flexDirection: 'column',
+    gap: 50,
+    backgroundColor: '#7308bb',
+    height: 200,
+  },
+  photoPerfil: {
+    borderRadius: 30,
+    width: 50,
+    height: 50,
+    marginLeft: 15
+  },
+  welcomeText: {
+    width: 200,
+    height: 200,
+    color: '#ffffff',
+    marginLeft: 25,
+    fontSize: 20,
+  },
+  images: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    marginTop: 50,
+  },
+  navIcon: {
+    gap: 8,
+    flexDirection: 'row',
+  },
+  eyeIcon: {
+    borderRadius: 30,
+    width: 35,
+    height: 35,
+    tintColor: '#ffffff',
+  },
+  infoConta: {
+    flexDirection: 'column',
+    textAlign: 'left',
+    marginLeft: 25,
+    marginTop: 30,
+  },
+  txtAccount: {
+    color: '#000000',
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  accountValue: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  txtIconContainer: {
+    justifyContent: 'space-between',
+    flexDirection: 'row'
+  },
+  setaIcon: {
+    width: 15,
+    height: 15,
+    marginRight: 20,
+    marginTop: 10,
+    tintColor: '#000000',
+  },
+  buttonsDiv: {
+    marginTop: 20,
+  },
+  buttons: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  buttonsIcons: {
+    alignItems: 'center',
+    width: 80,
+    gap: 15
+  },
+  buttonNav: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 50,
+    width: 70,
+    height: 70,
+    backgroundColor: '#928d8d49',
+  },
+  pagarButton: {
+    marginBottom: 13,
+    gap: 20
+  },
+  imgIcon: {
+    width: 47,
+    height: 47,
+    tintColor: '#000000',
+
+  }, pagarIcon: {
+    width: 20,
+    height: 17,
+    tintColor: '#000000',
+  },
+  buttonTxt: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    textAlign: 'center',
+    fontWeight: 'bold'
+  },
+  cardDiv: {
+    backgroundColor: '#928d8d49',
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 15,
+    height: 55,
+    width: 350,
+    marginLeft: 20,
+    marginTop: 20,
+  },
+  cardTxt: {
+    fontSize: 16,
+    fontWeight: '500'
+  }, imgCard: {
+    height: 55,
+    width: 50,
+    tintColor: '#000000',
+  },
+  line: {
+    backgroundColor: '#928d8d49',
+    height: 2,
+    opacity: 0.6,
+    marginTop: 20
+  },
+  infoCard: {
+    flexDirection: 'column',
+    textAlign: 'left',
+    marginBottom: 50,
+    gap: 10
+  },
+  txtIconContainer2: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginTop: 10
+  },
+  txtCard: {
+    color: '#000000',
+    fontSize: 22,
+    fontWeight: "bold",
+    marginLeft: 25,
+    marginTop: 5,
+  },
+  cardInfo: {
+    fontSize: 14,
+    marginLeft: 25,
+    color : '#6b6868f1',
+  },
+  cardValue: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginLeft: 25,
+  },
+});
